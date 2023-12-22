7ab85ba1c58b0aa9447d56fe2c0f48255132e473
JeremieLucca <jhenry@lucca.fr>
2023-12-22 16:46:48 +0100

    Majuscule sur la premier lettre des tags

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -2,6 +2,17 @@ import styles from "@/app/styles/postCard.module.css";
 import Link from "next/link";
 
 function PostCard({ post }) {
+  function capitalizeFirstLetter(string) {
+    if (typeof string !== 'string' || string.length === 0) {
+      return '';
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const capitalizedWorkplaceType = capitalizeFirstLetter(post.workplaceType || '');
+  const capitalizedCommitment = capitalizeFirstLetter(post.categories.commitment || '');
+  const capitalizedLocation = capitalizeFirstLetter(post.categories.location || '');
+
   return (
     <Link key={post.id} href={`/post/${post.id}`} className={styles.card}>
       <div className={styles.onHover}>
@@ -14,12 +25,13 @@ function PostCard({ post }) {
       </div>
       <p className={styles.post}>{post.text}</p>
       <div className={styles.more}>
-        <span>{post.categories.location}</span>
-        <span>{post.categories.commitment}</span>
-        <span>{post.workplaceType}</span>
+        <span>{capitalizedLocation}</span>
+        <span>{capitalizedCommitment}</span>
+        <span>{capitalizedWorkplaceType}</span>
       </div>
     </Link>
   );
 }
+
 
 export default PostCard;
