cb0073b2b1f4f95fbab5a72c32d64fb9d4391114
JeremieLucca <jhenry@lucca.fr>
2024-01-16 15:38:52 +0100

    test again

--- libs/getAllPosts.js
+++ libs/getAllPosts.js
@@ -1,15 +1,28 @@
+import { useEffect, useState, useCallback } from "react";
 import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const useFetchPosts = () => {
-  const { data, error } = useSWR(
+  const { data, error, revalidate } = useSWR(
     'https://api.lever.co/v0/postings/lucca?mode=json',
-    fetcher,
-    {
-      revalidateOnFocus: false, // Désactiver le cache
-    }
+    fetcher
   );
+
+  // Effectuer la revalidation lorsque la page devient visible
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        revalidate();
+      }
+    };
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, [revalidate]);
 
   return {
     data,
